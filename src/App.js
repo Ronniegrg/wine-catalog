f9d891b333e5e519b3c1111aa244f185e1e8d22c
Rouni Gorgees <rounigorgees@gmail.com>
2021-12-14 21:14:37 -0500

    create view to display the Catalog component or the About page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
-import Catalog from './Catalog';
+import React, { Component } from "react";
+import "./App.css";
+import Catalog from "./Catalog";
+import About from "./About";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <h1 className="App-title">The Cart App</h1>
-      </header>
-	  <Catalog />
+          <h1 className="App-title">The Cart App</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Catalog</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Catalog} />
+          <Route path="/about" component={About} />
+        </Switch>
+        <Catalog />
       </div>
     );
   }
