00fc744681aee9efa9b67f006d969dd139630646
Rouni Gorgees <rounigorgees@gmail.com>
2021-12-14 21:20:49 -0500

    add the import of a Link component and use it in the highlighted markup. The Link component allows us to create a hyperlink element that will be caught by the browser Router component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Catalog from "./Catalog";
 import About from "./About";
-import { Route, Switch } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">The Cart App</h1>
+          <h1 className="App-title">The Catalog App</h1>
           <nav>
             <ul>
               <li>
