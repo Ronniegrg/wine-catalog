d399bddb384b219d71daa90185f5bd608b5c2e7d
Rouni Gorgees <rounigorgees@gmail.com>
2021-12-14 20:53:16 -0500

    change cart multiple occurrences to the same product to single occurrence of a product and display its quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
 class CartItem extends React.Component {
   render() {
-    return <li className="cart-item">
-            <h3>{this.props.item.name}</h3>
-          </li>;
+    return (
+      <li className="cart-item">
+        <h3>
+          {this.props.item.name} ({this.props.item.quantity})
+        </h3>
+      </li>
+    );
   }
 }
 
--- src/CartItemList.js
+++ src/CartItemList.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './CartItemList.css';
-import CartItem from './CartItem';
+import React from "react";
+import "./CartItemList.css";
+import CartItem from "./CartItem";
 
 class CartItemList extends React.Component {
   render() {
     let items = [];
-    
-          for (let item of this.props.items) {
-              items.push(<CartItem item={item}/>);
-          }
+
+    for (let item of this.props.items) {
+      items.push(<CartItem item={item} />);
+    }
 
     return <ul>{items}</ul>;
   }
