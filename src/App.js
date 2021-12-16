20f74df4d1b155c001ff594b976a51c3f74b486c
Rouni Gorgees <rounigorgees@gmail.com>
2021-12-15 19:24:54 -0500

    add new link to the page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Catalog from "./Catalog";
 import About from "./About";
+import WineMakers from "./WineMakers";
 import { Link, Route, Switch } from "react-router-dom";
 
 class App extends Component {
@@ -16,6 +17,9 @@ class App extends Component {
                 <Link to="/">Catalog</Link>
               </li>
               <li>
+                <Link to="/winemakers">WineMakers</Link>
+              </li>
+              <li>
                 <Link to="/about">About</Link>
               </li>
             </ul>
@@ -23,6 +27,7 @@ class App extends Component {
         </header>
         <Switch>
           <Route exact path="/" component={Catalog} />
+          <Route path="/winemakers" component={WineMakers} />
           <Route path="/about" component={About} />
           {/*<Route path="/footer" children={() => <Footer />} />*/}
           {/*/!*<Route path="/about" render={() => <About />} />*!/ this line can be use instead of the line 26 and the result is the same*/}
